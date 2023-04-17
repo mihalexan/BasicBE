897cb1835c4259fcbc80d56a9eced8b685b2379f
Alexandra <125511128+mihalexan@users.noreply.github.com>
2023-04-17 16:12:27 +0200

    Delete Car.java

--- Car.java
+++
@@ -1,9 +0,0 @@
-public class Car {
-    double price = 0;
-    String category = "";
-
-    Car(double price, String category) {
-        this.price = price;
-        this.category = category;
-    }
-}
