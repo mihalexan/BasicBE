e513bcd45241838123160f9971970c4ac5cffe81
Alexandra <125511128+mihalexan@users.noreply.github.com>
2023-04-17 15:52:06 +0200

    HW27.03
    1 уровень сложности: Задание: 
    1) Создать класс "Account", который содержит в себе две переменные и конструктор для них:
    double currentAmount, String category.
     2) Создать классы Car, Phone, Plane, DownshifterKit с двумя переменными в каждом и конструкторами для них: double price, String category. 
    3) Создать класс “Main”, в нём метод “main”.  
    4) В методе “main” создать 1 объект класса “Account”, 2 объекта класса Car, 2 объекта класса Phone,  2 объекта класса Plane, 1  объекта класса DownshifterKit разными значениями “price”(“currentAmount” для объекта класса Account) у каждого.
     5) Заполнить переменные “category” у каждого объекта(включая объект класса Account) категориями(на выбор): “stylish”, “scientist”, “programmer”, “captain”. 
    6) В классе Main написать метод , который будет определять по переменным account.currentAmount и account.category c помощью конструкции if->else if… -> else, какой объект из созданных должен купить человек с текущим Account’oм.   
    Пример:   Если на счёте 1500 USD(account.currentAmount) и категория человека - “scientist”(account.category), то при выборе
      Plane(1500, “scientist”)
    Phone(1500, “captain”)
     DownshifterKit(0, “stylish”)
      метод должен будет выбрать “Plane(1500, “scientist”)” т.к. и цена и категория у этого объекта совпадает с тем, что содержится в аккаунте.

---
+++ HW27.03/AssabilityToBy_27.03/src/Phone.java
@@ -0,0 +1,9 @@
+public class Phone {
+    double price = 0;
+    String category = "";
+
+    Phone(double price, String category) {
+        this.price = price;
+        this.category = category;
+    }
+}
