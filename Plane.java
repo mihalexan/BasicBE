806ac866691f284e59276d433fbba33618701a46
Alexandra <125511128+mihalexan@users.noreply.github.com>
2023-04-17 16:08:21 +0200

    Delete Plane.java

--- Plane.java
+++
@@ -1,9 +0,0 @@
-public class Plane {
-    double price = 0;
-    String category = "";
-
-    Plane(double price, String category) {
-        this.price = price;
-        this.category = category;
-    }
-}
